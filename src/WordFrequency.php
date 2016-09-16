412998960642a1f35f5c465bfc7435de5f4b504a
Angie Smith <avksmit2@gmail.com>
2016-09-16 10:28:06 -0700

    change the way punctuation is handled, taking out the regular expression.

--- src/WordFrequency.php
+++ src/WordFrequency.php
@@ -4,9 +4,22 @@
         function countRepeats($phrase, $chosenWord)
         {
             $i = 0;
+            $punctuation = array(",", ".", "!", "''", ";", ":", "?");
+
             $words = explode(" ", $phrase);
             foreach ($words as $word) {
-                $word = preg_replace('/[^a-z0-9]+/i', '', $word);
+                // $word = preg_replace('/[^a-z0-9]+/i', '', $word);
+                $chars = str_split($word);
+                $j = 0;
+                foreach ($chars as $character) {
+                    if (in_array($character, $punctuation))
+                    {
+                        array_splice($chars,$j,1);
+                    }
+                    $j++;
+                }
+
+                $word = implode($chars);
 
                 if (strtolower($word) == strtolower($chosenWord))
                 {
